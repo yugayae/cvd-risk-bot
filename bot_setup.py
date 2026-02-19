fbcc8ff105af64440fe5c6d1ad0e095e49a17772
yugayae <yugayae@gmail.com>
2026-02-20 00:46:08 +0900

    Delete bot_setup.py

--- bot_setup.py
+++
@@ -1,7 +0,0 @@
-from app.bot_instance import dp
-from bot.handlers import common, form
-
-# Register Routers
-# This execution happens on import
-dp.include_router(common.router)
-dp.include_router(form.router)
